e2b08d7d7d928a277c8552d93b0ff5248049f3d5
Waingapucoder <r3ndydinar@gmail.com>
2022-09-19 15:19:12 +0800

    only use folder dist in library splide

--- public/splide-extension-auto-scroll-master/src/js/constants/i18n.ts
+++
@@ -1,4 +0,0 @@
-export const I18N = {
-  startScroll: 'Start auto scroll',
-  pauseScroll: 'Pause auto scroll',
-};
--- public/splide-extension-auto-scroll-master/src/js/index.ts
+++
@@ -1,2 +0,0 @@
-export { AutoScroll } from './extensions';
-export type { AutoScrollComponent } from './extensions';
